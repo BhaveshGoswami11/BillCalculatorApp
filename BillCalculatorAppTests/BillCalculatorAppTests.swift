753a9011798e4573b49e8969272549dd072e918d
Bhavesh Goswami <goswamjbhavesh@gmail.com>
2025-11-07 09:18:16 -0600

    Create BillCalculatorAppTests.swift

---
+++ BillCalculatorAppTests/BillCalculatorAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BillCalculatorAppTests.swift
+//  BillCalculatorAppTests
+//
+//  Created by Bhavesh on 10/23/25.
+//
+
+import Testing
+@testable import BillCalculatorApp
+
+struct BillCalculatorAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
